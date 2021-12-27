8036c493d38e94b6eeaf756ab7796ecb3247da6c
Jason Lyle <jason.lyle@oracle.com>
2021-12-27 09:46:09 -0500

    Updates to bootstrap process

--- generate_bootstrap.sh
+++ generate_bootstrap.sh
@@ -5,8 +5,6 @@ scriptDir="$(dirname "${BASH_SOURCE[0]}")"
 function main() {
     local filesDirName="files"
     local filesDir="${scriptDir}/${filesDirName}"
-    local sshDirName="ssh"
-    local sshDir="${filesDir}/${sshDirName}"
     local gpgDirName="gpg"
     local gpgDir="${filesDir}/${gpgDirName}"
     local bootstrapScriptName="bootstrap.sh"
@@ -22,12 +20,15 @@ function main() {
     mkdir "${bootstrapDir}"
 
     cp -a "${bootstrapScript}" "${bootstrapDir}"
-    cp -a "${sshDir}" "${bootstrapDir}"
     cp -a "${gpgDir}" "${bootstrapDir}"
 
     #TODO: Remove this for final version.. this is just to make testing on
     #TODO: the vm easier
+    if [ -f "${bootstrapDirName}.iso" ]; then
+        rm "${bootstrapDirName}.iso"
+    fi
     hdiutil makehybrid -iso -joliet -o "${bootstrapDirName}" "${bootstrapDir}"
+    #TODO: End remove block
 
     return 0
 }
